980c42c35a56ea06a8f0e154cbfecdbf269edd9b
Josh <dominiclopes2804@gmail.com>
2023-02-04 17:32:55 +0530

    Added select_tag

--- antlia/app/controllers/reviews_controller.rb
+++ antlia/app/controllers/reviews_controller.rb
@@ -22,7 +22,6 @@ class ReviewsController < ApplicationController
   # POST /reviews or /reviews.json
   def create
     @review = Review.new(review_params)
-
     respond_to do |format|
       if @review.save
         format.html { redirect_to review_url(@review), notice: "Review was successfully created." }
